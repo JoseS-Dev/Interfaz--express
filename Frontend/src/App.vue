1225124771ca08850111330abf6b0822c942db08
Alvarezzzzz <alejandrojalvarezg.2005@gmail.com>
2025-05-29 16:16:48 -0400

    feat: Se agrega el enrrutado de las vistas

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,20 +1,14 @@
 <script setup>
+import { RouterView } from 'vue-router';
 import Navbar from './components/Navbar.vue';
 import LoginModal from './components/LoginModal.vue';
-import Banner from './components/Banner.vue';
-import Services from './components/Services.vue';
-import Carrusel from './components/Carrusel.vue';
-import ContactForm from './components/ContactForm.vue';
 import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Navbar />
   <LoginModal />
-  <Banner />
-  <Services />
-  <Carrusel />
-  <ContactForm />
+  <RouterView />
   <Footer />
 </template>
 
---
+++ Frontend/src/views/Home.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Banner from '@/components/Banner.vue';
+import Services from '@/components/Services.vue';
+import Carrusel from '@/components/Carrusel.vue';
+import ContactForm from '@/components/ContactForm.vue';
+</script>
+
+<template>
+    <Banner />
+    <Services />
+    <Carrusel />
+    <ContactForm />
+</template>
