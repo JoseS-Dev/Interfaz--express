e09efdc1b07cfc2032260ff39881b6526e533337
Alvarezzzzz <alejandrojalvarezg.2005@gmail.com>
2025-05-29 13:10:24 -0400

    feat(Home): se agregaron todos los componentes junto con sus funcionalidades
    Navbar, banner, Services, Carrusel, ContactForm, Footer

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,8 +1,20 @@
 <script setup>
-
+import Navbar from './components/Navbar.vue';
+import LoginModal from './components/LoginModal.vue';
+import Banner from './components/Banner.vue';
+import Services from './components/Services.vue';
+import Carrusel from './components/Carrusel.vue';
+import ContactForm from './components/ContactForm.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
-  <h1 class="bg-red-500">Hola</h1>
+  <Navbar />
+  <LoginModal />
+  <Banner />
+  <Services />
+  <Carrusel />
+  <ContactForm />
+  <Footer />
 </template>
 
---
+++ Frontend/src/components/ImgCarruselMobile.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    imgUrl: {
+        type: String,
+        required: true,
+    },
+});
+</script>
+
+<template>
+    <div class="w-full flex-shrink-0">
+        <img :src="imgUrl" :alt="title" class="w-full h-64 sm:h-80 object-cover rounded-lg">
+    </div>
+</template>
