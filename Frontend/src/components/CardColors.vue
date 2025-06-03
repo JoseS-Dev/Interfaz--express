033a125af3300fb5755f76dad7ec66214bb8357d
Alvarezzzzz <alejandrojalvarezg.2005@gmail.com>
2025-06-03 10:10:35 -0400

    style: agregando estilos al preview de colores

--- Frontend/src/components/CardColors.vue
+++ Frontend/src/components/CardColors.vue
@@ -3,11 +3,16 @@
 </script>
 
 <template>
-    <div class="text-center rounded-lg border-2 border-black w-full h-110 flex flex-col items-center justify-center my-5">
-        <div class="w-32 h-32 mx-auto mb-4 rounded-full overflow-hidden shadow-lg">
-            <img src="../assets/img/img1.avif" alt="Consulta Médica" class="w-full h-full object-cover">
+    <div class="w-full bg-primary py-2">
+        <div class="text-center rounded-lg h-110 flex flex-col items-center justify-center my-5 bg-quaternary w-17/20 m-auto shadow-lg border-2 border-primary">
+            <div class="w-32 h-32 mx-auto mb-4 rounded-full overflow-hidden shadow-lg">
+                <img src="../assets/img/img1.avif" alt="Consulta Médica" class="w-full h-full object-cover">
+            </div>
+            <h3 class="text-2xl font-semibold text-secondary mb-2">Consulta Médica</h3>
+            <p class="text-quinary text-xl">Atención médica personalizada con profesionales especializados</p>
+            <button class="mt-8 bg-tertiary text-quaternary font-medium py-3 px-6 rounded-md transition-colors cursor-pointer text-xl">
+                    Agendar Cita
+            </button>
         </div>
-        <h3 class="text-xl font-semibold text-secondary mb-2">Consulta Médica</h3>
-        <p class="text-quinary">Atención médica personalizada con profesionales especializados</p>
     </div>
 </template>
