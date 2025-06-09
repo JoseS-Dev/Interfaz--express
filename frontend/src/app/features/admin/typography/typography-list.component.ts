fe6c5f43db7b02432e8d440167cf3ebe6f9ea087
alemt19 <alfredomt1906@gmail.com>
2025-06-08 21:25:55 -0400

    feat: Añadir que no se pueda borrar la tipografía por defecto (la que tenga id = 1 xdd)

--- frontend/src/app/features/admin/typography/typography-list.component.ts
+++ frontend/src/app/features/admin/typography/typography-list.component.ts
@@ -32,16 +32,18 @@ import { environment } from "../../../../environments/environment";
                         <span class="font-bold" title="tam_subtitle">{{ typography.tam_subtitle }}px</span>
                         <span class="font-bold" title="tam_paragraph">{{ typography.tam_paragraph }}px</span>
                     </div>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" (click)="handleDelete(typography.id_tipography, $event)"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round"
-                            class="lucide lucide-trash2-icon lucide-trash-2 hover:text-red-600 transition-colors duration-300 cursor-pointer">
-                            <path d="M3 6h18" />
-                            <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                            <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-                            <line x1="10" x2="10" y1="11" y2="17" />
-                            <line x1="14" x2="14" y1="11" y2="17" />
-                    </svg>
+                    @if (typography.id_tipography != '1') {
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" (click)="handleDelete(typography.id_tipography, $event)"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="lucide lucide-trash2-icon lucide-trash-2 hover:text-red-600 transition-colors duration-300 cursor-pointer">
+                                <path d="M3 6h18" />
+                                <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                                <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                                <line x1="10" x2="10" y1="11" y2="17" />
+                                <line x1="14" x2="14" y1="11" y2="17" />
+                        </svg>
+                    }
                 </div>
             }
         </article>
