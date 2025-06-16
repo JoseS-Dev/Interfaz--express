0adc6d53c8fe31583d755b6a11e86cfa421e78ea
JoSantana0005 <joseasantana05@gmail.com>
2025-06-16 11:01:36 -0400

    style: :art: Agregando la pagina de admin al proyecto en react

--- Plantilla en React/src/App.tsx
+++ Plantilla en React/src/App.tsx
@@ -1,24 +1,17 @@
-
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Services from './components/Services';
-import Gallery from './components/Gallery';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import LoginModal from './components/LoginModal';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './Pages/Home';
+import Admin from './Pages/Admin';
 
 function App() {
   return (
-    <div className="bg-primary">
-      <Navbar />
-      <LoginModal />
-      <Hero />
-      <Services />
-      <Gallery />
-      <Contact />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/admin' element={<Admin />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ Plantilla en React/src/Pages/Home.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from '../components/Navbar';
+import Hero from '../components/Hero';
+import Services from '../components/Services';
+import Gallery from '../components/Gallery';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
+import LoginModal from '../components/LoginModal';
+
+const Home = () =>{
+    return (
+        <div className="bg-primary">
+            <Navbar />
+            <LoginModal />
+            <Hero />
+            <Services />
+            <Gallery />
+            <Contact />
+            <Footer />
+        </div>
+    )
+}
+export default Home;
