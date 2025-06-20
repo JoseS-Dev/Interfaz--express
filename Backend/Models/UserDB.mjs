0523d20608b91d242e4eaa6a0581f3fc9490d128
JoSantana0005 <joseasantana05@gmail.com>
2025-06-20 11:32:45 -0400

    refactor: :recycle: Mejorando la logica de los modelos , me quedo bello

--- Backend/Models/UserDB.mjs
+++ Backend/Models/UserDB.mjs
@@ -161,7 +161,16 @@ export class ModelsUsers{
 
     // Obtener a todos los usuarios
     static async getAllUsers(){
-        const [users] = await connection.query('SELECT * FROM user_register');
+        const [users] = await connection.query(
+            `SELECT a.*, b.*, c.*, d.*, e.*, f.*
+            FROM user_register a 
+            JOIN info_user b ON a.id_user = b.id_user
+            JOIN address_user c ON a.id_user = c.id_user
+            JOIN bank_info_user d ON a.id_user = d.id_user
+            JOIN companies_user e ON a.id_user = e.id_user
+            JOIN crypto_wallets_user f ON a.id_user = f.id_user
+            ORDER BY a.id_user DESC`
+        );
         if(users.length > 0){
             console.log("Usuaurios obtenidos");
             return users;
@@ -175,7 +184,26 @@ export class ModelsUsers{
     // Obtener a un usuario por su ID
     static async getUserByID({id_user}){
         if(id_user){
-            const [userID] = await connection.query("SELECT * FROM user_register WHERE id_user = ?", [id_user]);
+            const [userID] = await connection.query(
+                `SELECT a.name_user, a.maiden_name_user, a.email_user, a.username,
+                b.age_user, b.phone_user, b.birth_date_user, b.image_user, b.blood_group_user,
+                b.height_user, b.weight_user, b.eye_color_user, b.ip_user, b.mac_address_user,
+                b.university_user, b.ein_user, b.ssn_user, b.user_agent_user, b.role_user,
+                c.street_address, c.city_address, c.state_address, c.state_code_address,
+                c.postal_code_address, c.latitude_address, c.longitude_address, c.country_address,
+                d.card_expire_user, d.card_number_user, d.card_type_user, d.currency_user,
+                d.iban_user, e.department_company_user, e.company_name_user, e.company_title_user,
+                e.company_street_user, e.company_city_user, e.company_state_user, e.company_state_code_user,
+                e.company_postal_code_user, e.company_latitude_user, e.company_longitude_user,
+                e.company_country_user, f.coin_user, f.wallet_address_user, f.network_user
+                FROM user_register a JOIN info_user b ON a.id_user = b.id_user
+                JOIN address_user c ON a.id_user = c.id_user
+                JOIN bank_info_user d ON a.id_user = d.id_user
+                JOIN companies_user e ON a.id_user = e.id_user
+                JOIN crypto_wallets_user f ON a.id_user = f.id_user
+                WHERE a.id_user = ?`,
+                [id_user]
+            );
             if(userID.length > 0){
                 console.log("Usuario encontrado con el ID solicitado");
                 return userID;
@@ -220,15 +248,99 @@ export class ModelsUsers{
     // Actualizar un usuario
     static async updateUser({id_user, user}){
         if(!id_user || !user) return null;
-        const {name_user, email_user, password_user, username} = user;
+        const {
+            name_user, maiden_name_user, email_user, password_user, username,
+            age_user, phone_user, birth_date_user, image_user, blood_group_user,
+            height_user, weight_user, eye_color_user, ip_user, mac_address_user,
+            university_user, ein_user, ssn_user, user_agent_user, role_user,
+            street_address, city_address, state_address, state_code_address,
+            postal_code_address, latitude_address, longitude_address, country_address,
+            card_expire_user, card_number_user, card_type_user, currency_user,
+            iban_user, department_company_user, company_name_user, company_title_user,
+            company_street_user, company_city_user, company_state_user, company_state_code_user,
+            company_postal_code_user, company_latitude_user, company_longitude_user,
+            company_country_user, coin_user, wallet_address_user, network_user
+
+        } = user;
+        
         // Se verifica si existe un usuario con ese ID
         const [existingUser] = await connection.query('SELECT * FROM user_register WHERE id_user = ?', [id_user]);
         if(existingUser.length > 0){
             const hashedPassword = password_user ? await bcrypt.hash(password_user, 10) : existingUser[0].password_user;
-            const [updateUser] = await connection.query('UPDATE user_register SET name_user = ?, email_user = ?, password_user = ?, username = ? WHERE id_user = ?', [name_user, email_user, hashedPassword, username, id_user]);
+            const [updateUser] = await connection.query('UPDATE user_register SET name_user = ?,maiden_name_user = ?, email_user = ?, password_user = ?, username = ? WHERE id_user = ?', [name_user,maiden_name_user, email_user, hashedPassword, username, id_user]);
             if(updateUser.affectedRows > 0){
-                console.log('Usuario actualizado');
-                return updateUser;
+                console.log("Cambion en el registro del usuario");
+                // Se actualizan los datos adicionales del usuario en la tabla info_user
+                const [updateInfoUser] = await connection.query(
+                    `UPDATE info_user 
+                    SET age_user = ?, phone_user = ?, birth_date_user = ?, image_user = ?, blood_group_user = ?,
+                    height_user = ?, weight_user = ?, eye_color_user = ?, ip_user = ?, mac_address_user = ?,
+                    university_user = ?, ein_user = ?, ssn_user = ?, user_agent_user = ?, role_user = ?
+                    WHERE id_user = ?`,
+                    [
+                        age_user, phone_user, birth_date_user, image_user, blood_group_user,
+                        height_user, weight_user, eye_color_user, ip_user, mac_address_user,
+                        university_user, ein_user, ssn_user, user_agent_user, role_user,
+                        id_user
+                    ]
+                );
+                console.log("Datos personales del usuario actualizados");
+                // Se actualiza la direccion del usuario en la tabla address_user
+                const [updatedAddressUser] = await connection.query(
+                    `UPDATE address_user 
+                    SET street_address = ?, city_address = ?, state_address = ?, state_code_address = ?,
+                    postal_code_address = ?, latitude_address = ?, longitude_address = ?, country_address = ?
+                    WHERE id_user = ?`,
+                    [
+                        street_address, city_address, state_address, state_code_address,
+                        postal_code_address, latitude_address, longitude_address, country_address,
+                        id_user
+                    ]
+                )
+                console.log("Direccion del usuario actualizada");
+                // Se actualizan los datos de la tarjeta del usuario en la tabla bank_info_user
+                const [updatedBankUser] = await connection.query(
+                    `UPDATE bank_info_user 
+                    SET card_expire_user = ?, card_number_user = ?, card_type_user = ?, currency_user = ?,
+                    iban_user = ?
+                    WHERE id_user = ?`,
+                    [
+                        card_expire_user, card_number_user, card_type_user, currency_user,
+                        iban_user, id_user
+                    ]
+                )
+                console.log("Datos de la tarjeta del usuario actualizados");
+                // Se actualizan los datos de la empresa del usuario en la tabla companies_user
+                const [updatedCompanyUser] = await connection.query(
+                    `UPDATE companies_user 
+                    SET department_company_user = ?, company_name_user = ?, company_title_user = ?,
+                    company_street_user = ?, company_city_user = ?, company_state_user = ?, company_state_code_user = ?,
+                    company_postal_code_user = ?, company_latitude_user = ?, company_longitude_user = ?,
+                    company_country_user = ?
+                    WHERE id_user = ?`,
+                    [
+                        department_company_user, company_name_user, company_title_user,
+                        company_street_user, company_city_user, company_state_user, company_state_code_user,
+                        company_postal_code_user, company_latitude_user, company_longitude_user,
+                        company_country_user, id_user
+                    ]
+                )
+                console.log("Datos de la empresa del usuario actualizados");
+                // Se actualizan los datos de la billetera del usuario en la tabla crypto_wallet_user
+                const [updatedWalletUser] = await connection.query(
+                    `UPDATE crypto_wallets_user 
+                    SET coin_user = ?, wallet_address_user = ?, network_user = ?
+                    WHERE id_user = ?`,
+                    [
+                        coin_user, wallet_address_user, network_user, id_user
+                    ]
+                );
+                console.log("Datos de la billetera del usuario actualizados");
+                // Se verifica que se hayan actualizado todos los datos
+                if(updateInfoUser.affectedRows > 0 && updatedAddressUser.affectedRows > 0 && updatedBankUser.affectedRows > 0 && updatedCompanyUser.affectedRows > 0 && updatedWalletUser.affectedRows > 0){
+                    console.log("Usuario actualizado con exito");
+                    return updateUser;
+                }
             }
             else{
                 console.log('Error al actualizar el usuario');
